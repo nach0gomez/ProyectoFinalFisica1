87bf9a37c0a9f6deb5a93066d80b54003eb0a1cc
Nacho Gomez <terminatitorxdyt@gmail.com>
2023-04-27 14:56:55 -0500

    implementacion de la grafica de preuba

--- v3.py
+++ v3.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 # Crear la ventana principal
 root = tk.Tk()
@@ -8,7 +11,7 @@ root = tk.Tk()
 root.resizable(0,0)
 root.title("Calculadora de Enfriamiento de Componentes")
 root['background']='#569DAA'
-width = 700 # Width 
+width = 760 # Width 
 height = 280 # Height
 
 screen_width = root.winfo_screenwidth()  
@@ -43,12 +46,9 @@ label2.pack(pady=10)
 label2.place(x=20, y=60)
 label2['background']='#569DAA'
 
-style= ttk.Style()
-style.theme_use('clam')
-style.configure("TCombobox", fieldbackground= "orange", background= "white")
 
 
-# Crear un menú desplegable con opciones
+# Crear un menú desplegable con cada componente
 options = ["Seleccione una opcion","Intel Core i5-10300H", "Ryzen 5 5600H", "Intel Core i7-12700K"]
 combo = ttk.Combobox(root, values=options, state="readonly")
 combo.current(0)
@@ -58,32 +58,33 @@ combo['background']='#577D86'
 
 
 
-# Crear un cuadro de imagen que cambie según la opción seleccionada
+# Crea el cuadro de imagen que cambieas egún la opción seleccionada
 image1 = Image.open("i5.png")  # reemplazar con la ubicación de su propia imagen
-resized_image1= image1.resize((275,197), Image.LANCZOS) # * 275,183 medidas fijas
+resized_image1= image1.resize((90, 70), Image.LANCZOS) # * 275,183 medidas fijas
 photo1 = ImageTk.PhotoImage(resized_image1)
 image2 = Image.open("ryzen 5.png")  # reemplazar con la ubicación de su propia imagen
-resized_image2= image2.resize((275,197), Image.LANCZOS)
+resized_image2= image2.resize((110, 80), Image.LANCZOS)
 photo2 = ImageTk.PhotoImage(resized_image2)
 image3 = Image.open("i7.png")  # reemplazar con la ubicación de su propia imagen\
-resized_image3= image3.resize((275,197), Image.LANCZOS) # * 275,183 medidas fijas   
+resized_image3= image3.resize((110, 80), Image.LANCZOS) # * 275,183 medidas fijas   
 photo3 = ImageTk.PhotoImage(resized_image3)
 image4 = Image.open("empty.png")  # reemplazar con la ubicación de su propia imagen
-resized_image4= image4.resize((275,197), Image.LANCZOS) # * 275,183 medidas fijas
+resized_image4= image4.resize((110, 80), Image.LANCZOS) # * 275,183 medidas fijas
 photo4 = ImageTk.PhotoImage(resized_image4)
 
 
-# Crear un label para el cuadro de imagen
+# Crea un label para el cuadro de imagen
 label3 = tk.Label(root, text="Componente seleccionado:")
 label3.pack(pady=10)
 label3.place(x=400, y=20)
 label3['background']='#569DAA'
 
-image_label = tk.Label(root, image=photo4)
-image_label.pack()
-image_label.place(x=400, y=50)
+# Ponemos la imagen empty como la predeteminada
+label_visual = tk.Label(root, image=photo4)
+label_visual.pack()
+label_visual.place(x=560, y=20, width= 90, height=70)
 
-# * funcion paa cambiar de imagen
+# * funcion para cambiar de imagen
 def change_image(event):
     selection = combo.get()
     
@@ -93,21 +94,21 @@ def change_image(event):
     label4['background']='#569DAA'
         
     if selection == "Seleccione una opcion":
-        image_label.configure(image=photo4)
-        label4.config(text="")
+        label_visual.configure(image=photo4)
+        label4.config(text="                                                                                                    ")
         
     elif selection == "Intel Core i5-10300H":
-        image_label.configure(image=photo1)
+        label_visual.configure(image=photo1)
         label4.config(text="El tiempo que tardará " + selection + " en enfriar es:")
         label4.place(x=21, y=170)
         
     elif selection == "Ryzen 5 5600H":
-        image_label.configure(image=photo2)
+        label_visual.configure(image=photo2)
         label4.config(text="El tiempo que tardará " + selection + " en enfriar es:                 ")
          
     elif selection == "Intel Core i7-12700K":
         label4.config(text="El tiempo que tardará " + selection + " en enfriar es:")
-        image_label.configure(image=photo3)
+        label_visual.configure(image=photo3)
 
 
 combo.bind("<<ComboboxSelected>>", change_image)
@@ -129,7 +130,7 @@ label5.place(x=20, y=100)
 label5['background']='#569DAA'
 
 
-# Crear una función para guardar el dato ingresado
+# Crear una función para guardar el dato ingresado en el entry
 def calcular():
     data = entry1.get()
     output.configure(state="normal")
@@ -138,13 +139,29 @@ def calcular():
     output.configure(state="disabled")
 
 # Crear un botón para guardar el dato ingresado
-button1 = tk.Button(root, text="Calcular", command=calcular, )
-button1.pack(pady=10)
-button1.place(x=20, y=130)
-button1['background']='#577D86'
+calcular = tk.Button(root, text="Calcular", command=calcular, )
+calcular.pack(pady=10)
+calcular.place(x=20, y=130)
+calcular['background']='#577D86'
 
 
+# Tratando de generar la grafica
+valores = [6, 5, 4, 3, 2, 1, 0]
 
+# Crear una figura y una gráfica con los valores
+fig, ax = plt.subplots()
+ax.plot(valores)
 
+# Crear un widget Frame para contener la gráfica
+grafica = tk.Frame(root)
+grafica.pack()
+grafica.place(x=400, y=50, width=300, height=200)
+
+# Crear el widget de Tkinter que contendrá la gráfica
+canvas = FigureCanvasTkAgg(fig, master=grafica)
+canvas.draw()
+
+# Posicionar la gráfica en el widget utilizando el método pack()
+canvas.get_tk_widget().pack()
 # Iniciar la ventana principal
 root.mainloop()
